45e372483329ccc667e87639861f1c2cd7ec9a64
Sgnahore <stephaniergnahore@gmail.com>
2023-07-24 14:33:40 +0100

    json file added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { greet } from "./utils/greet";
+import episodes from "./episodes.json"
+
+console.log(`Imported ${episodes.length} episode(s)`);
+console.log(`First episode's name is ${episodes[0].name}`);
 
 function App(): JSX.Element {
-  return <h1>{greet("Worldlings!!")}</h1>;
+  return <h1>{greet("World")}</h1>;
 }
 
 export default App;
