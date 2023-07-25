e33edcfa0bd44b212dc8c5e10dba2309038a7819
Sgnahore <stephaniergnahore@gmail.com>
2023-07-25 15:56:53 +0100

    First and second episodes rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import episodes from "./episodes.json"
+import { IEpisode } from "./IEpisodeTypes";
+import episodes from "./episodes.json";
 //import { IEpisode } from "./IEpisodeTypes";
 
 // console.log(`Imported ${episodes.length} episode(s)`);
 // console.log(`First episode's name is ${episodes[0].name}`);
 
+
 function App(): JSX.Element {
-
-  const names: string[] = [];
-  episodes.forEach((episode) => {
-    names.push(episode.name);
-  });
-
-  
-
   return (
     <>
-    {names}
+      <EpisodeCard episode={episodes[0]} />
+      <EpisodeCard episode={episodes[1]} />
     </>
+  );
+}
+
+interface EpisodeCardProps {
+  episode: IEpisode;
+}
+
+function EpisodeCard(props: EpisodeCardProps): JSX.Element {
+  return (
+  <>
+  <div>
+    {props.episode.name}
+    {props.episode.summary}
+
+  </div>
+  </>
   )
 }
 
