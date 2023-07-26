553395acfa4cbff7d0eb78a105c515d005e62bb7
Sgnahore <stephaniergnahore@gmail.com>
2023-07-26 15:44:21 +0100

    Start of episodeCode function

--- src/components/EpisodeCard.tsx
+++ src/components/EpisodeCard.tsx
@@ -1,24 +1,29 @@
-import { IEpisode } from "../IEpisodeTypes";
-import "../styles.css"
-interface EpisodeCardProps {
-    episode: IEpisode;
-  }
-  
+import { IEpisode } from "./IEpisodeTypes";
+import "../styles.css";
+import episodes from "../episodes.json";
 
-  function EpisodeCard(props: EpisodeCardProps): JSX.Element {
-    return (
-      <>
+
+interface EpisodeCardProps {
+  episode: IEpisode;
+}
+
+function EpisodeCard(props: EpisodeCardProps): JSX.Element {
+
+    // function episodeCode(episodes: EpisodeCardProps){
+    // }
+
+return (
+    <>
       <div className="gridContainer">
         <div className="episodeFrame">
           <p>{props.episode.name}</p>
-          <p>S {props.episode.season}</p>
-          <p>E {props.episode.number}</p>
+          <p>S {props.episode.season}E {props.episode.number}</p>
           <img src={props.episode.image.medium} alt="episode" />
           <p>{props.episode.summary}</p>
         </div>
       </div>
-      </>
-    );
-  }
+    </>
+  );
+}
 
-  export default EpisodeCard;
+export default EpisodeCard;
