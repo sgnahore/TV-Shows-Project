e5bcf7ba6b8df1d199e328fd4cd34bf59b964462
Sgnahore <stephaniergnahore@gmail.com>
2023-07-27 13:34:52 +0100

    season and episode code added

--- src/components/EpisodeCard.tsx
+++ src/components/EpisodeCard.tsx
@@ -2,22 +2,45 @@ import { IEpisode } from "./IEpisodeTypes";
 import "../styles.css";
 import episodes from "../episodes.json";
 
-
 interface EpisodeCardProps {
   episode: IEpisode;
 }
 
+function seasonCode(season: number): string {
+  let seasonCode: string;
+
+  if (season < 10) {
+    seasonCode = "S0" + season;
+  } else {
+    seasonCode = "S" + season;
+  }
+
+  return seasonCode;
+}
+
+function episodeCode(episodeNum: number): string {
+  let episodeCode: string;
+
+  if (episodeNum < 10) {
+    episodeCode = "E0" + episodeNum;
+  } else {
+    episodeCode = "E" + episodeNum;
+  }
+  return episodeCode;
+}
+
+// console.log("here is the season code", EpisodeCode());
+
 function EpisodeCard(props: EpisodeCardProps): JSX.Element {
-
-    // function episodeCode(episodes: EpisodeCardProps){
-    // }
-
-return (
+  return (
     <>
       <div className="gridContainer">
         <div className="episodeFrame">
           <p>{props.episode.name}</p>
-          <p>S {props.episode.season}E {props.episode.number}</p>
+          <p>
+            {seasonCode(props.episode.season)}
+            {episodeCode(props.episode.number)}
+          </p>
           <img src={props.episode.image.medium} alt="episode" />
           <p>{props.episode.summary}</p>
         </div>
