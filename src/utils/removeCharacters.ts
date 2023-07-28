ff5e592a519193a7c5e9f5ba219752f6d946aa5f
virtutae <virtutae@yahoo.it>
2023-07-28 11:37:51 +0100

    level 100 completed

--- src/utils/removeCharacters.ts
+++ src/utils/removeCharacters.ts
@@ -4,7 +4,7 @@ export default function removeCharacters(text:string):string {
 
 
 
-const something = /<p>|<\/p>/g
+const something = /<p>|<\/p>|<br>/g
 
     const trimmedText = text.replace(something, "")
 
