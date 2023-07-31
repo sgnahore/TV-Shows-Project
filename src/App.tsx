00c502c2739be1e730d19fa445710a9bf2a95e52
Sgnahore <stephaniergnahore@gmail.com>
2023-07-31 14:21:30 +0100

    Files arranged

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { IEpisode } from "./components/IEpisodeTypes";
-import episodes from "./episodes.json";
-import EpisodeCard from "./components/EpisodeCard";
+import MainContent from "./components/MainContent";
 import { Footer } from "./components/Footer";
 
-// console.log(`Imported ${episodes.length} episode(s)`);
-// console.log(`First episode's name is ${episodes[0].name}`);
-
-function App(): JSX.Element {
-  const allEpisodes = episodes.map((episode: IEpisode) => (
-    <EpisodeCard key={episode.id} episode={episode} />
-  ));
-
+export default function App(): JSX.Element {
   return (
     <>
-      {allEpisodes}
+      <MainContent />
       <Footer />
     </>
   );
 }
-
-export default App;
