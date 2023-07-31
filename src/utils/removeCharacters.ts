03915b7acd4503a64d68375a89a172e55af65926
Sgnahore <stephaniergnahore@gmail.com>
2023-07-31 14:23:20 +0100

    linted and formatted

--- src/utils/removeCharacters.ts
+++ src/utils/removeCharacters.ts
@@ -1,12 +1,7 @@
+export default function removeCharacters(text: string): string {
+  const something = /<p>|<\/p>|<br>/g;
 
+  const trimmedText = text.replace(something, "");
 
-export default function removeCharacters(text:string):string {
-
-
-
-const something = /<p>|<\/p>|<br>/g
-
-    const trimmedText = text.replace(something, "")
-
-return trimmedText
+  return trimmedText;
 }
